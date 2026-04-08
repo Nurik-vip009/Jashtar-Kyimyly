c87aaf0e324bbf763c8869a9122d0773b161541c
Zh1kaa <ryskulovazibek21@gmail.com>
2026-04-09 01:48:40 +0600

    Footer

--- src/app/api/apiclient.ts
+++ src/app/api/apiclient.ts
@@ -1,19 +1,20 @@
 import axios from "axios";
 
-const language = localStorage.getItem("lang") || "ru";
+const language =
+	typeof window !== "undefined" ? localStorage.getItem("lang") || "ru" : "ru";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://jashtar-kyimyly-production.up.railway.app/api/",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "*/*",
-    "ngrok-skip-browser-warning": "true",
-    "Accept-Language": language,
-  },
+	baseURL: "http://157.230.235.0/api/",
+	headers: {
+		"Content-Type": "application/json",
+		Accept: "*/*",
+		// УДАЛИТЕ ИЛИ ЗАКОММЕНТИРУЙТЕ СТРОКУ НИЖЕ:
+		// "ngrok-skip-browser-warning": "true",
+		"Accept-Language": language,
+	},
 });
 
-// Функция для смены языка динамически
 export const setLanguage = (lang: "ky" | "ru" | "en") => {
-  localStorage.setItem("lang", lang); // сохраняем выбранный язык
-  axiosInstance.defaults.headers["Accept-Language"] = lang; // обновляем хедер
+	localStorage.setItem("lang", lang);
+	axiosInstance.defaults.headers["Accept-Language"] = lang;
 };
